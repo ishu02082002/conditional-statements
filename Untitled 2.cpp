cdece3a32575f65b18b455a90ad2c27bd3256f22
ishu02082002 <75948270+ishu02082002@users.noreply.github.com>
2020-12-14 19:57:58 +0530

    program

---
+++ Untitled 2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int marks;
+	printf("marks");
+	scanf("%d",&marks);
+	if(marks>=85&&marks<=100)
+	printf("Grade A");
+	else if(marks>=70&&marks<85)
+	printf("Grade B");
+	else if(marks>=55&&marks<70)
+	printf("Grade C");
+	else if(marks>=40&&marks<55)
+	printf("Grade D");
+	else
+	printf("Grade F");
+}
+
